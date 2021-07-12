ea5af9c41e2cbe4da8dff474b0d96c72556d4a69
jubesbot <jerald.lim.93@gmail.com>
2021-07-13 01:22:33 +0800

    end of mon night, mapped dropdown and plotted charts

--- client/src/Components/ChartsView.js
+++ client/src/Components/ChartsView.js
@@ -7,141 +7,102 @@ import axios from "axios";
 
 function ChartsView({talentData, setTalentData, user, setUser}) {
     const [allSports, setAllSports] = useState({})
-    const [sportData, setSportData] = useState({})
+    const [sportId, setSportId] = useState('61')
+    const [sportData, setSportData] = useState({
+        id: 61,
+        sport: "Boxing",
+        attribute_endurance: 8.63,
+        attribute_strength: 8.13,
+        attribute_power: 8.63,
+        attribute_agility: 6.25,
+        attribute_flexibility: 4.38,
+        attribute_nerve: 8.88,
+        attribute_durability: 8.50,
+        attribute_handeye_coordination: 7.00,
+        attribute_analytic_aptitude: 5.63,
+        total: 72.38,
+    })
 
     //nothing yet
 
     //to work with the form selector
-    async function getOneSport() {
-        let sportName = ''
+
+    async function getOneSport(e) {
+        e.preventDefault()
         try {
-            let {data} = await Axios.get(`http://localhost:8000/sports/?sport=${sportName}`)
+            let {data} = await axios.get(`http://localhost:8000/sports/${sportId}`)
             console.log(data)
+            setSportData(data)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    // function change(e) {
+    //     setSportData(() => (e.target.value))
+    //     console.log(sportData)
+    // }
+
+    //renders one sport for the chart on page load
+
+    async function getAllSports() {
+
+        try {
+            let {data} = await axios.get(`http://localhost:8000/sports/`)
+            setAllSports(data)
         } catch (e) {
             console.log(e.response)
         }
     }
 
-    //renders one sport for the chart on page load
+    // async function getSport() {
+    //     try {
+    //         let {data} = await axios.get(`http://localhost:8000/sports/${sportId}`)
+    //         setSportData(data)
+    //     } catch (e) {
+    //         console.log(e.response)
+    //     }
+    // }
+
     useEffect(() => {
-        let sportName = 'Archery'
 
-        async function getSport() {
-            try {
-                let {data} = await axios.get(`http://localhost:8000/sports/?sport=${sportName}`)
-                setSportData(data)
-            } catch (e) {
-                console.log(e.response)
-            }
-        }
-        getSport()
+        // getSport()
 
-        async function getAllSports() {
-            try {
-                let {data} = await axios.get(`http://localhost:8000/sports/`)
-                setAllSports(data)
-            } catch (e) {
-                console.log(e.response)
-            }
-        }
         getAllSports()
 
     }, [])
 
-    console.log('SINGLE SPORT' + " " + JSON.stringify(sportData))
-    console.log('ALL SPORTS' + " " + JSON.stringify(allSports))
-
-    // const data = {
-    //     labels: [
-    //         'Endurance',
-    //         'Strength',
-    //         'Power',
-    //         'Speed',
-    //         'Agility',
-    //         'Flexibility',
-    //         'Nerve',
-    //         'Durability',
-    //         'Hand-eye Coordination',
-    //         'Analytical Aptitude'
-    //     ],
-    //     datasets: [
-    //         {
-    //             label: 'Score',
-    //             data: [1, 3, 4, 3, 2, 3, 4, 2, 1, 2],
-    //             backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    //             borderColor: 'rgba(255, 99, 132, 1)',
-    //             borderWidth: 1,
-    //         },
-    //     ],
-    // };
-    //
-    // const options = {
-    //     scale: {
-    //         ticks: {beginAtZero: true},
-    //     },
-    // };
+    console.log(sportId)
+    console.log(sportData)
+    console.log(allSports.length)
 
     return (
         <div>
 
-            {/*<Row xs={2} md={3} lg={4} className='border border-warning'>*/}
-
-            {/*     <Col className='col-6 border border-primary mx-auto'>*/}
-            {/*         <div className='header'>*/}
-            {/*             <h3 className='title'>Pick a Sport:</h3>*/}
-            {/*         </div>*/}
-            {/*         <Radar data={data} options={options}/>*/}
-
-            {/*     </Col>*/}
-
-            {/*     <Col className='col-6 border border-primary mx-auto'>*/}
-
-            {/*         <div className='header'>*/}
-            {/*             <h3 className='title'>Talent's Stats:</h3>*/}
-            {/*         </div>*/}
-            {/*         <Radar data={data} options={options}/>*/}
-
-            {/*     </Col>*/}
-
-            {/* </Row>*/}
-
-            {/* <Row xs={2} md={3} lg={4} className='border border-warning'>*/}
-
-            {/*     <Col className='col-6 border border-primary mx-auto'>*/}
-            {/*         <div className='header'>*/}
-            {/*             <h3 className='title'>Suggested Sport:</h3>*/}
-            {/*         </div>*/}
-            {/*         <Radar data={data} options={options}/>*/}
-
-            {/*     </Col>*/}
-
-            {/* </Row>*/}
-
-            {/* <button onClick={() => getTalents()}>Get Talents</button>*/}
-            {/* <button onClick={() => getSports()}>Get Sports</button>*/}
             <Row xs={2} md={2} lg={3} className='border border-warning'>
                 <Col className='col-6 border border-primary mx-auto'>
-                    <Form>
+                    <Form method="get" onSubmit={getOneSport}>
                         <Row>
                             <Col>
                                 <Form.Group controlId="exampleForm.ControlSelect1">
                                     <Form.Label className='h4'>Select Sport</Form.Label>
-                                    <Form.Control as="select" name="sport">
-
-                                        {/*{allSports.map((sport) => (*/}
-                                        {/*    <option value={sport.id}>{sport.sport}</option>*/}
-                                        {/*))}*/}
-
-                                        <option>avc</option>
-                                        <option>avc</option>
+                                    <Form.Control as="select" value={sportId} onChange={e => {
+                                        console.log("e.target.value", typeof e.target.value);
+                                        setSportId(e.target.value);
+                                    }}>
+                                        {(allSports.length > 0) &&
+                                        allSports.map((sport) => (
+                                            <option key={sport.id} value={sport.id}>{sport.sport}</option>
+                                        ))}
                                     </Form.Control>
                                 </Form.Group>
-                                <button type="submit" className="btn border-dark text-center btn-sm"
-                                        onClick={() => getOneSport()}>Get Sport
+                                <button type="submit" className="btn border-dark text-center btn-sm">Get Sport
                                 </button>
                             </Col>
                         </Row>
                     </Form>
+
+                    {(sportId > 0) &&
                     <Radar
                         width={500} height={500} padding={70} domainMax={10} highlighted={null} onHover={(point) => {
                         if (point) {
@@ -168,21 +129,22 @@ function ChartsView({talentData, setTalentData, user, setUser}) {
                                     key: 'sport',
                                     label: 'Sport',
                                     values: {
-                                        attribute_endurance: 10,
-                                        attribute_strength: 9,
-                                        attribute_power: 3,
-                                        attribute_speed: 4,
-                                        attribute_agility: 5,
-                                        attribute_flexibility: 3,
-                                        attribute_nerve: 2,
-                                        attribute_durability: 8,
-                                        attribute_handeye_coordination: 9,
-                                        attribute_analytic_aptitude: 4,
+                                        attribute_endurance: sportData.attribute_endurance,
+                                        attribute_strength: sportData.attribute_strength,
+                                        attribute_power: sportData.attribute_power,
+                                        attribute_speed: sportData.attribute_speed,
+                                        attribute_agility: sportData.attribute_agility,
+                                        attribute_flexibility: sportData.attribute_flexibility,
+                                        attribute_nerve: sportData.attribute_nerve,
+                                        attribute_durability: sportData.attribute_durability,
+                                        attribute_handeye_coordination: sportData.attribute_handeye_coordination,
+                                        attribute_analytic_aptitude: sportData.attribute_analytic_aptitude,
                                     },
                                 },
                             ],
                         }}
                     />
+                    }
                 </Col>
 
                 <Col className='col-6 border border-primary mx-auto'>
