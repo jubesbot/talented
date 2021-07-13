0b8652af670849802292669b11e61badaed85abd
jubesbot <jerald.lim.93@gmail.com>
2021-07-13 17:23:12 +0800

    able to suggest sports to user

--- client/src/Components/ChartsSuggestedView.js
+++ client/src/Components/ChartsSuggestedView.js
@@ -1,88 +1,180 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Radar from "react-d3-radar";
-import {Col} from "react-bootstrap";
+import {Button, Col} from "react-bootstrap";
 
-function ChartsSuggestedView({sportData, setSportData, allSports, setAllSports, sportId, setSportId, talentData, allTalents, setAllTalents, talentId, setTalentData, setTalentId}) {
-    return (
-        <div>
+function ChartsSuggestedView({
+                                 sportData,
+                                 setSportData,
+                                 allSports,
+                                 setAllSports,
+                                 sportId,
+                                 setSportId,
+                                 talentData,
+                                 allTalents,
+                                 setAllTalents,
+                                 talentId,
+                                 setTalentData,
+                                 setTalentId
+                             }) {
 
-            <div className='header'>
-                <h4 className='title'>Suggested Sport:</h4>
-            </div>
-            <div>
-                <h2>blank space</h2>
-                <h3>blank space</h3>
-            </div>
+    const [search, setSearch] = useState({})
 
-            {(sportData && talentData && talentData.id) ?
-            <Radar width={400} height={400} padding={70} domainMax={10} highlighted={null} onHover={(point) => {
-                if (point) {
-                    console.log('hovered over a data point');
-                } else {
-                    console.log('not over anything');
-                }
-            }}
-                   data={{
-                       variables: [
-                           {key: 'attribute_endurance', label: 'Endurance'},
-                           {key: 'attribute_strength', label: 'Strength'},
-                           {key: 'attribute_power', label: 'Power'},
-                           {key: 'attribute_speed', label: 'Speed'},
-                           {key: 'attribute_agility', label: 'Agility'},
-                           {key: 'attribute_flexibility', label: 'Flexibility'},
-                           {key: 'attribute_nerve', label: 'Nerve'},
-                           {key: 'attribute_durability', label: 'Durability'},
-                           {key: 'attribute_handeye_coordination', label: 'Hand-eye Coordination'},
-                           {key: 'attribute_analytic_aptitude', label: 'Analytic Aptitude'},
-                       ],
-                       sets: [
-                           {
-                               key: '',
-                               label: '',
-                               values: {},
-                           },
-                           {
-                               key: 'talent',
-                               label: 'Talent',
-                               values: {
-                                   attribute_endurance: talentData.attribute_endurance,
-                                   attribute_strength: talentData.attribute_strength,
-                                   attribute_power: talentData.attribute_power,
-                                   attribute_speed: talentData.attribute_speed,
-                                   attribute_agility: talentData.attribute_agility,
-                                   attribute_flexibility: talentData.attribute_flexibility,
-                                   attribute_nerve: talentData.attribute_nerve,
-                                   attribute_durability: talentData.attribute_durability,
-                                   attribute_handeye_coordination: talentData.attribute_handeye_coordination,
-                                   attribute_analytic_aptitude: talentData.attribute_analytic_aptitude,
-                               },
-                           },
-                           {
-                               key: 'sport',
-                               label: 'Sport',
-                               values: {
-                                   attribute_endurance: 10,
-                                   attribute_strength: 9,
-                                   attribute_power: 3,
-                                   attribute_speed: 4,
-                                   attribute_agility: 5,
-                                   attribute_flexibility: 3,
-                                   attribute_nerve: 2,
-                                   attribute_durability: 8,
-                                   attribute_handeye_coordination: 9,
-                                   attribute_analytic_aptitude: 4,
-                               },
-                           },
+    function filterSearch() {
+        let temp = []
 
-                       ],
-                   }}
-
-            />
-                : <>Rendering...</>
+        for (let i = 0; i < allSports.length; i++) {
+            if (Math.abs((talentData.attribute_endurance) - (allSports[i].attribute_endurance)) < 2){
+                temp.push(allSports[i].sport)
+            }
+            if (Math.abs((talentData.attribute_strength) - (allSports[i].attribute_strength)) < 2){
+                temp.push(allSports[i].sport)
+            }
+            if (Math.abs((talentData.attribute_power) - (allSports[i].attribute_power)) < 2){
+                temp.push(allSports[i].sport)
+            }
+            if (Math.abs((talentData.attribute_speed) - (allSports[i].attribute_speed)) < 2){
+                temp.push(allSports[i].sport)
+            }
+            if (Math.abs((talentData.attribute_agility) - (allSports[i].attribute_agility)) < 2){
+                temp.push(allSports[i].sport)
+            }
+            if (Math.abs((talentData.attribute_flexibility) - (allSports[i].attribute_flexibility)) < 2){
+                temp.push(allSports[i].sport)
+            }
+            if (Math.abs((talentData.attribute_nerve) - (allSports[i].attribute_nerve)) < 2){
+                temp.push(allSports[i].sport)
+            }
+            if (Math.abs((talentData.attribute_durability) - (allSports[i].attribute_durability)) < 2){
+                temp.push(allSports[i].sport)
+            }
+            if (Math.abs((talentData.attribute_handeye_coordination) - (allSports[i].attribute_handeye_coordination)) < 2){
+                temp.push(allSports[i].sport)
+            }
+            if (Math.abs((talentData.attribute_analytic_aptitude) - (allSports[i].attribute_analytic_aptitude)) < 2){
+                temp.push(allSports[i].sport)
             }
 
+            // if (talentData.attribute_strength - 1 > allSports[i].attribute_strength) {
+            //     temp.push(allSports[i].sport)
+            // }
+            // if (talentData.attribute_power - 1 > allSports[i].attribute_power) {
+            //     temp.push(allSports[i].sport)
+            // }
+            // if (talentData.attribute_speed - 1 > allSports[i].attribute_speed) {
+            //     temp.push(allSports[i].sport)
+            // }
+            // if (talentData.attribute_agility - 1 > allSports[i].attribute_agility) {
+            //     temp.push(allSports[i].sport)
+            // }
+            // if (talentData.attribute_flexibility - 1 > allSports[i].attribute_flexibility) {
+            //     temp.push(allSports[i].sport)
+            // }
+            // if (talentData.attribute_nerve - 1 > allSports[i].attribute_nerve) {
+            //     temp.push(allSports[i].sport)
+            // }
+            // if (talentData.attribute_durability - 1 > allSports[i].attribute_durability) {
+            //     temp.push(allSports[i].sport)
+            // }
+            // if (talentData.attribute_handeye_coordination - 1 > allSports[i].attribute_handeye_coordination) {
+            //     temp.push(allSports[i].sport)
+            // }
+            // if (talentData.attribute_analytic_aptitude - 1 > allSports[i].attribute_analytic_aptitude) {
+            //     temp.push(allSports[i].sport)
+            // }
+
+        }
+
+         function suggestedSports(arr,count){
+            return [...new Set(arr)].filter(x =>
+                arr.filter(a => a === x).length >= count
+            )
+        }
+        setSearch(suggestedSports(temp,6))
+    }
+    console.log(search)
+
+return (
+    <div>
+        <Button onClick={filterSearch}>Button</Button>
+        <div className='header'>
+            <h4 className='title'>Based on {talentData.talent_name}'s top attributes,<br/> the suggested sports are:</h4>
+            {(search && search.length > 0) &&
+            search.map((sport) => (
+                <ul key={sport.id}>{sport}</ul>
+            ))}
         </div>
-    );
+        <div>
+        </div>
+
+        {/*{(sportData && talentData && talentData.id) ?*/}
+        {/*    <Radar width={400} height={400} padding={70} domainMax={10} highlighted={null} onHover={(point) => {*/}
+        {/*        if (point) {*/}
+        {/*            console.log('hovered over a data point');*/}
+        {/*        } else {*/}
+        {/*            console.log('not over anything');*/}
+        {/*        }*/}
+        {/*    }}*/}
+        {/*           data={{*/}
+        {/*               variables: [*/}
+        {/*                   {key: 'attribute_endurance', label: 'Endurance'},*/}
+        {/*                   {key: 'attribute_strength', label: 'Strength'},*/}
+        {/*                   {key: 'attribute_power', label: 'Power'},*/}
+        {/*                   {key: 'attribute_speed', label: 'Speed'},*/}
+        {/*                   {key: 'attribute_agility', label: 'Agility'},*/}
+        {/*                   {key: 'attribute_flexibility', label: 'Flexibility'},*/}
+        {/*                   {key: 'attribute_nerve', label: 'Nerve'},*/}
+        {/*                   {key: 'attribute_durability', label: 'Durability'},*/}
+        {/*                   {key: 'attribute_handeye_coordination', label: 'Hand-eye Coordination'},*/}
+        {/*                   {key: 'attribute_analytic_aptitude', label: 'Analytic Aptitude'},*/}
+        {/*               ],*/}
+        {/*               sets: [*/}
+        {/*                   {*/}
+        {/*                       key: '',*/}
+        {/*                       label: '',*/}
+        {/*                       values: {},*/}
+        {/*                   },*/}
+        {/*                   {*/}
+        {/*                       key: 'talent',*/}
+        {/*                       label: 'Talent',*/}
+        {/*                       values: {*/}
+        {/*                           attribute_endurance: talentData.attribute_endurance,*/}
+        {/*                           attribute_strength: talentData.attribute_strength,*/}
+        {/*                           attribute_power: talentData.attribute_power,*/}
+        {/*                           attribute_speed: talentData.attribute_speed,*/}
+        {/*                           attribute_agility: talentData.attribute_agility,*/}
+        {/*                           attribute_flexibility: talentData.attribute_flexibility,*/}
+        {/*                           attribute_nerve: talentData.attribute_nerve,*/}
+        {/*                           attribute_durability: talentData.attribute_durability,*/}
+        {/*                           attribute_handeye_coordination: talentData.attribute_handeye_coordination,*/}
+        {/*                           attribute_analytic_aptitude: talentData.attribute_analytic_aptitude,*/}
+        {/*                       },*/}
+        {/*                   },*/}
+        {/*                   {*/}
+        {/*                       key: 'sport',*/}
+        {/*                       label: 'Sport',*/}
+        {/*                       values: {*/}
+        {/*                           attribute_endurance: 10,*/}
+        {/*                           attribute_strength: 9,*/}
+        {/*                           attribute_power: 3,*/}
+        {/*                           attribute_speed: 4,*/}
+        {/*                           attribute_agility: 5,*/}
+        {/*                           attribute_flexibility: 3,*/}
+        {/*                           attribute_nerve: 2,*/}
+        {/*                           attribute_durability: 8,*/}
+        {/*                           attribute_handeye_coordination: 9,*/}
+        {/*                           attribute_analytic_aptitude: 4,*/}
+        {/*                       },*/}
+        {/*                   },*/}
+
+        {/*               ],*/}
+        {/*           }}*/}
+
+        {/*    />*/}
+        {/*    : <>Rendering...</>*/}
+        {/*}*/}
+
+    </div>
+);
 }
 
 export default ChartsSuggestedView;
